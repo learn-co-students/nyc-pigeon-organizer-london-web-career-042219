0b602dde88b98b41dbf931cd045f9aa85c3c89ff
Marco Miranda <marmirand@hotmail.com>
2019-04-13 11:01:29 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |colors_gener_lives, inner_value|
+    inner_value.each do |key, array|
+      array.each do |e|
+        if new_hash[e] == nil
+          new_hash[e] = {}
+        end
+        new_hash[e][colors_gener_lives] = []
+      end
+    end
+  end
+  data.each do |colors_gener_lives, inner_value|
+    inner_value.each do |key, array|
+      array.each do |e|
+        if array.include?(e)
+          new_hash[e][colors_gener_lives] << key.to_s
+        end
+      end
+    end
+  end
+  new_hash
 end
