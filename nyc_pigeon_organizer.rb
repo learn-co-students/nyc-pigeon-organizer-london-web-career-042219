8ad4e63c4e4ec663a3fb77a0886bdb86a1687e7b
rossmawd <rossmawd@gmail.com>
2019-04-21 17:08:35 +0100

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,35 @@
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+
+pigeon_list = {}
+ data.each {|category,hash|
+   hash.each {|att,names|
+     names.select {|person|
+       pigeon_list[person] = {}
+     }
+   }
+ }
+ # collect the categories in an array(colour, gender, lives)
+  cats = []
+  data.each {|category,hash|
+    cats << category
+  }
+ #adds above categories to each pigeon's hash as keys
+  pigeon_list.each {|name,hash|
+    cats.each {|symbol|
+      hash[symbol] = []
+    }
+  }
+  #iterate through data: if value array contains name
+  #add attribute to approppriate category
+  pigeon_list.each {|person,hash|
+    data.each {|category,hash|
+      hash.each {|att,names|
+        if names.include?(person)
+          pigeon_list[person][category] << att.to_s
+        end
+      }
+    }
+  }
+pigeon_list
 end
